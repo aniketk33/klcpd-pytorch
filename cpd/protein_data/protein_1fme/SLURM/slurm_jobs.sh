bf75b6bb14aecc56f76ff603d1155311dc3a0393
Aniket Kumar <aniketkumar693@gmail.com>
2023-07-24 16:56:33 -0400

    sbatch scripts added for 1fme dataset

--- cpd/protein_data/protein_1fme/SLURM/slurm_jobs.sh
+++ cpd/protein_data/protein_1fme/SLURM/slurm_jobs.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-sbatch -o /hpcgpfs01/scratch/akumar/code/cpd/protein_data/protein_19ht/SLURM/OUTPUT/random_2.out -p voltadebug -t 02:30:00 --gres=gpu:1 -J 19ht_r_2 -A student-v /hpcgpfs01/scratch/akumar/code/cpd/protein_data/protein_19ht/SLURM/random_2.sh
+sbatch -o /hpcgpfs01/scratch/akumar/code/cpd/protein_data/protein_1fme/SLURM/OUTPUT/random_2.out --nodes=2 -p voltadebug -t 12:30:00 --gres=gpu:2 -J 1fme_r_2 -A student-v /hpcgpfs01/scratch/akumar/code/cpd/protein_data/protein_1fme/SLURM/random_2.sh
 
-sbatch -o /hpcgpfs01/scratch/akumar/code/cpd/protein_data/protein_19ht/SLURM/OUTPUT/random_3.out -p voltadebug -t 02:30:00 --gres=gpu:1 -J 19ht_r_3 -A student-v /hpcgpfs01/scratch/akumar/code/cpd/protein_data/protein_19ht/SLURM/random_3.sh
+sbatch -o /hpcgpfs01/scratch/akumar/code/cpd/protein_data/protein_1fme/SLURM/OUTPUT/random_3.out --nodes=2 -p voltadebug -t 12:30:00 --gres=gpu:2 -J 1fme_r_3 -A student-v /hpcgpfs01/scratch/akumar/code/cpd/protein_data/protein_1fme/SLURM/random_3.sh
 
-sbatch -o /hpcgpfs01/scratch/akumar/code/cpd/protein_data/protein_19ht/SLURM/OUTPUT/random_4.out -p voltadebug -t 02:30:00 --gres=gpu:1 -J 19ht_r_4 -A student-v /hpcgpfs01/scratch/akumar/code/cpd/protein_data/protein_19ht/SLURM/random_4.sh
+sbatch -o /hpcgpfs01/scratch/akumar/code/cpd/protein_data/protein_1fme/SLURM/OUTPUT/random_4.out --nodes=2 -p voltadebug -t 12:30:00 --gres=gpu:2 -J 1fme_r_4 -A student-v /hpcgpfs01/scratch/akumar/code/cpd/protein_data/protein_1fme/SLURM/random_4.sh
 
-sbatch -o /hpcgpfs01/scratch/akumar/code/cpd/protein_data/protein_19ht/SLURM/OUTPUT/svds_2.out -p voltadebug -t 02:30:00 --gres=gpu:1 -J 19ht_s_2 -A student-v /hpcgpfs01/scratch/akumar/code/cpd/protein_data/protein_19ht/SLURM/svds_2.sh
+sbatch -o /hpcgpfs01/scratch/akumar/code/cpd/protein_data/protein_1fme/SLURM/OUTPUT/svds_2.out --nodes=2 -p voltadebug -t 12:30:00 --gres=gpu:2 -J 1fme_s_2 -A student-v /hpcgpfs01/scratch/akumar/code/cpd/protein_data/protein_1fme/SLURM/svds_2.sh
 
-sbatch -o /hpcgpfs01/scratch/akumar/code/cpd/protein_data/protein_19ht/SLURM/OUTPUT/svds_3.out -p voltadebug -t 02:30:00 --gres=gpu:1 -J 19ht_s_3 -A student-v /hpcgpfs01/scratch/akumar/code/cpd/protein_data/protein_19ht/SLURM/svds_3.sh
+sbatch -o /hpcgpfs01/scratch/akumar/code/cpd/protein_data/protein_1fme/SLURM/OUTPUT/svds_3.out --nodes=2 -p voltadebug -t 12:30:00 --gres=gpu:2 -J 1fme_s_3 -A student-v /hpcgpfs01/scratch/akumar/code/cpd/protein_data/protein_1fme/SLURM/svds_3.sh
