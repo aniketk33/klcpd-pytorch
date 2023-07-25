d7c64f1c4baa05224bed3dcd4cfc3212dc92c262
Aniket Kumar <aniketkumar693@gmail.com>
2023-07-25 13:22:10 -0400

    SLURM output files and scripts

--- cpd/protein_data/SLURM/test.sh
+++ cpd/protein_data/SLURM/test.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-# SBATCH --job-name=bash
-# SBATCH --partition=voltadebug
-# SBATCH --account=student-v
-# SBATCH --chdir=/hpcgpfs01/scratch/akumar/code/cpd/
+#SBATCH -J aniket_job
+#SBATCH --partition voltadebug
+#SBATCH -A student-v
+#SBATCH -o /hpcgpfs01/scratch/akumar/code/cpd/protein_data/SLURM/aniket_job.out
+#SBATCH --gres=gpu:1
+# SBATCH --checkpoint 00:01:00
+# SBATCH --checkpoint-dir=/hpcgpfs01/scratch/akumar/code/cpd/protein_data/SLURM/aniket_job.ckpt
 
 source ~/.bashrc
 conda activate cuda_env
-python /hpcgpfs01/scratch/akumar/code/cpd/protein_data/protein_19ht/random_2.py
+python /hpcgpfs01/scratch/akumar/code/cpd/protein_data/test-klcpd.py
+python /hpcgpfs01/scratch/akumar/code/cpd/protein_data/test-klcpd.py
