442d4ecda98d0ad158eb31f7f81db58a7d78d3bb
Aniket Kumar <aniketkumar693@gmail.com>
2023-07-26 12:06:59 -0400

    SLURM output files

--- cpd/protein_data/protein_19ht/SLURM/slurm_jobs.sh
+++ cpd/protein_data/protein_19ht/SLURM/slurm_jobs.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+#SBATCH -o /hpcgpfs01/scratch/akumar/code/cpd/protein_data/protein_19ht/SLURM/slurm_jobs.out
+#SBATCH -p voltadebug
+#SBATCH -t 02:30:00
+#SBATCH --gres=gpu:1
+#SBATCH -A student-v
+#SBATCH -J slurm_jobs
 
 sbatch /hpcgpfs01/scratch/akumar/code/cpd/protein_data/protein_19ht/SLURM/random_2.sh
 
