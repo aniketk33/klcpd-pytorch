f316c03e12291f13a4f9bec18e2fd43b1c2e00ce
Aniket Kumar <aniketkumar693@gmail.com>
2023-07-27 11:59:45 -0400

    partition chanes for 19fe dataset

--- cpd/protein_data/protein_1fme/SLURM/svds_3.sh
+++ cpd/protein_data/protein_1fme/SLURM/svds_3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -o /hpcgpfs01/scratch/akumar/code/cpd/protein_data/protein_1fme/SLURM/OUTPUT/svds_3.out
-#SBATCH -p voltadebug
+#SBATCH -p volta
 #SBATCH -t 02:30:00
 #SBATCH --gres=gpu:1
 #SBATCH -A student-v
