fd7d98b9f4b212b967f15ab9a4099bd53bbdddd8
Aniket Kumar <aniketkumar693@gmail.com>
2023-07-27 12:36:24 -0400

    testing env variable using sbatch

--- cpd/protein_data/SLURM/test.sh
+++ cpd/protein_data/SLURM/test.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-#SBATCH -J aniket_job
-#SBATCH --partition voltadebug
-#SBATCH -A student-v
-#SBATCH -o /hpcgpfs01/scratch/akumar/code/cpd/protein_data/SLURM/aniket_job.out
-#SBATCH --gres=gpu:1
-# SBATCH --checkpoint 00:01:00
-# SBATCH --checkpoint-dir=/hpcgpfs01/scratch/akumar/code/cpd/protein_data/SLURM/aniket_job.ckpt
-
-source ~/.bashrc
-conda activate cuda_env
-python /hpcgpfs01/scratch/akumar/code/cpd/protein_data/test-klcpd.py
-python /hpcgpfs01/scratch/akumar/code/cpd/protein_data/test-klcpd.py
+set -a
+source $ENV_FILE/.env
+echo $PARTITION
