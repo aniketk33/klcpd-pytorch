a0bc6f65b1c715c0e23d2604d9ee25c080698f32
Aniket Kumar <aniketkumar693@gmail.com>
2023-07-28 10:06:23 -0400

    code fixes

--- cpd/protein_data/protein_19ht/random_4.py
+++ cpd/protein_data/protein_19ht/random_4.py
@@ -1,9 +1,10 @@
 from utils.klcpd_main import get_reduced_data, train_and_pred_dataset, save_preds
 from protein_data.prepare_data import get_coordinates
 
-data = get_coordinates('PROTEIN_19HT')
+dataset_name = 'PROTEIN_19HT'
+data = get_coordinates(dataset_name)
 svd_method = 'random'
 components = 4
 data_reduced = get_reduced_data(data, components, svd_method)
-preds = train_and_pred_dataset(data_reduced, 'protein_19ht', svd_method, components)
-save_preds(data_reduced, preds, svd_method, svd_method)
+preds = train_and_pred_dataset(data_reduced, dataset_name.lower(), svd_method, components)
+save_preds(data_reduced, preds, svd_method, dataset_name.lower())
